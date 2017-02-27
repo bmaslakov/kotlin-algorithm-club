283e7ce2c086cafc5005ce405d95288f1d1110b2
Boris Maslakov <b.maslakov@gmail.com>
2017-02-27 23:14:40 +0300

    add(data-structures): Stack

---
+++ src/im/uuddlrlrba/ktalgs/datastructures/StackTest.kt
@@ -0,0 +1,31 @@
+package im.uuddlrlrba.ktalgs.datastructures
+
+import org.junit.Assert
+import org.junit.Test
+import java.util.*
+
+class StackTest {
+    @Test
+    public fun emptyTest() {
+        val stack = Stack<Int>()
+        Assert.assertEquals(0, stack.size)
+    }
+
+    @Test(expected=NoSuchElementException::class)
+    public fun exceptionTest() {
+        val stack = Stack<Int>()
+        stack.peek()
+    }
+
+    @Test
+    public fun naiveTest() {
+        val stack = Stack<Int>()
+        for (i in 0..10) {
+            stack.push(i)
+        }
+        for (i in 10..0) {
+            Assert.assertEquals(i, stack.peek())
+            Assert.assertEquals(i, stack.poll())
+        }
+    }
+}
