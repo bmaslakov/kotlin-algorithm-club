eb2ea3164d96667dd4df75a1daf8dab5b087874a
Boris Maslakov <b.maslakov@gmail.com>
2017-03-05 19:25:53 +0300

    add(graphs): BFS

---
+++ src/main/io/uuddlrlrba/ktalgs/graphs/BFS.kt
@@ -0,0 +1,29 @@
+package io.uuddlrlrba.ktalgs.graphs
+
+import io.uuddlrlrba.ktalgs.datastructures.Queue
+
+class BFS {
+    companion object Implementations {
+        fun iterative(graph: Graph, op: (Int) -> Unit) {
+            val visited = BooleanArray(graph.V)
+            val queue = Queue<Int>()
+            for (i in 0..graph.V - 1) {
+                if (!visited[i]) {
+                    queue.add(i)
+                    visited[i] = true
+                    op(i)
+                    while (!queue.isEmpty()) {
+                        val v = queue.poll()
+                        for (w in graph.adjacentVertices(v)) {
+                            if (!visited[w]) {
+                                queue.add(w)
+                                visited[w] = true
+                                op(i)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
