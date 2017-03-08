6e4e68270402cf98a6b199928e1e144e21d9f90a
Boris Maslakov <b.maslakov@gmail.com>
2017-03-08 15:20:08 +0300

    test(sorts): more cases

--- src/test/io/uuddlrlrba/ktalgs/sorts/AbstractSortTest.kt
+++ src/test/io/uuddlrlrba/ktalgs/sorts/AbstractSortTest.kt
@@ -19,8 +19,36 @@ abstract class AbstractSortTest<out T: AbstractSortStrategy>(val strategy: T) {
     }
 
     @Test
-    fun naiveTest() {
+    fun twoElementsInOrderTest() {
+        val arr = arrayOf(42, 43)
+        strategy.perform(arr)
+        Assert.assertArrayEquals(arrayOf(42, 43), arr)
+    }
+
+    @Test
+    fun twoElementsOutOfOrderTest() {
+        val arr = arrayOf(43, 42)
+        strategy.perform(arr)
+        Assert.assertArrayEquals(arrayOf(42, 43), arr)
+    }
+
+    @Test
+    fun twoElementsEqualTest() {
+        val arr = arrayOf(42, 42)
+        strategy.perform(arr)
+        Assert.assertArrayEquals(arrayOf(42, 42), arr)
+    }
+
+    @Test
+    fun tenElementsReverseTest() {
         val arr = arrayOf(10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+        strategy.perform(arr)
+        Assert.assertArrayEquals(arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), arr)
+    }
+
+    @Test
+    fun tenElementsTest() {
+        val arr = arrayOf(3, 2, 7, 6, 1, 8, 10, 9, 4, 5)
         strategy.perform(arr)
         Assert.assertArrayEquals(arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), arr)
     }
