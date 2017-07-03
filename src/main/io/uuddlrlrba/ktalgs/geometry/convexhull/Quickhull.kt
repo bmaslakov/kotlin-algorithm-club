dcdbb9f5c8ea63ed020a21a0b0ec9af78d0ed366
Boris Maslakov <b.maslakov@gmail.com>
2017-07-03 14:42:35 +0300

    fix(geometry/convex-hull): cleaner quickhull implementation

--- src/main/io/uuddlrlrba/ktalgs/geometry/convexhull/Quickhull.kt
+++ src/main/io/uuddlrlrba/ktalgs/geometry/convexhull/Quickhull.kt
@@ -22,57 +22,26 @@
 
 package io.uuddlrlrba.ktalgs.geometry.convexhull
 
-import io.uuddlrlrba.ktalgs.datastructures.Stack
 import io.uuddlrlrba.ktalgs.geometry.Point
 
 class Quickhull: ConvexHullAlgorithm {
     override fun convexHull(points: Array<Point>): Collection<Point> {
         if (points.size < 3) throw IllegalArgumentException("there must be at least 3 points")
-
-        val hull = Stack<Point>()
-
-        // Find the leftmost point
         val left = points.min()!!
         val right = points.max()!!
-
-        // add left half
-        for (point in quickHull(points.asList(), left, right)) {
-            hull.push(point)
-        }
-
-        // add right half
-        for (point in quickHull(points.asList(), right, left)) {
-            hull.push(point)
-        }
-
-        return hull
+        return quickHull(points.asList(), left, right) + quickHull(points.asList(), right, left)
     }
 
     private fun quickHull(points: Collection<Point>, first: Point, second: Point): Collection<Point> {
         val pointsLeftOfLine = points
                 .filter { it.isLeftOfLine(first, second) }
                 .map { Pair(it, it.distanceToLine(first, second)) }
-
-        val hull = Stack<Point>()
-
         if (pointsLeftOfLine.isEmpty()) {
-            hull.push(second)
+            return listOf(second)
         } else {
             val max = pointsLeftOfLine.maxBy { it.second }!!.first
-
-            val newPoints = Stack<Point>()
-            for (pd in pointsLeftOfLine) {
-                newPoints.push(pd.first)
-            }
-
-            for (point in quickHull(newPoints, first, max)) {
-                hull.push(point)
-            }
-            for (point in quickHull(newPoints, max, second)) {
-                hull.push(point)
-            }
+            val newPoints = pointsLeftOfLine.map { it.first }
+            return quickHull(newPoints, first, max) + quickHull(newPoints, max, second)
         }
-
-        return hull
     }
 }
